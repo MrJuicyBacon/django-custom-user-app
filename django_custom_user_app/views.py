0947e54bb55d483ea03672c62b30009ef18860ef
MrJuicyBacon <iambacon@yandex.ru>
2019-12-02 23:11:14 +0300

    create_response() added
    Helper function to get HttpResponse with provided data

--- django_custom_user_app/views.py
+++ django_custom_user_app/views.py
@@ -6,19 +6,29 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import User, Session as DbSession
 
 
+def create_response(input_object, status_code=200, content_type="application/json"):
+    return HttpResponse(json.dumps(input_object), content_type=content_type, status=status_code)
+
+
 @csrf_exempt
 @require_POST
 def auth(request):
+    # Check if all fields are present
     if not ('email' in request.POST and 'password' in request.POST):
-        return HttpResponse('NO FIELDS')
+        return create_response({'error': '"email" and "password" fields are required.'}, 400)
+
+    # Get user object and check if user exists
     user = DbSession.query(User).filter(User.email == request.POST['email']).first()
     if user is None:
-        return HttpResponse('NO USER')
-    if check_password(request.POST['password'], user.password) is not True:
-        return HttpResponse('WRONG PASS')
+        return create_response({'error': 'No user found with provided email.'}, 400)
 
+    # Check password
+    if check_password(request.POST['password'], user.password) is not True:
+        return create_response({'error': 'Wrong password.'}, 400)
+
+    # All checks passed, creating a response
     response_data = {
         'user': user.as_dict(),
         'auth_token': None,
     }
-    return HttpResponse(json.dumps(response_data), content_type="application/json")
+    return create_response(response_data)
